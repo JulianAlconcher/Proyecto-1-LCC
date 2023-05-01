754270ef5d94db330860e499c25ba11f6246b1b0
TobiasThiessen11 <tobiasthiessen@live.com.ar>
2023-05-01 18:10:00 -0300

    Grid ya funcionando para tamaño variable

--- pengines_server/apps/proylcc/proylcc.pl
+++ pengines_server/apps/proylcc/proylcc.pl
@@ -12,7 +12,6 @@
 		RGrids = [[0 | Resto], [N2 | Resto]].
 */
 	
-
 join(Grid, NumOfColumns, Path, RGrids):-
     lista_de_posiciones(Path,NumOfColumns,Posiciones),
     sort(Posiciones,PosicionesOrdenadas),
@@ -21,15 +20,10 @@ join(Grid, NumOfColumns, Path, RGrids):-
     ultimo(Ultimo,Posiciones),
     truncar_a_potencia_de_2(Suma, Resultado),
     convertir_en_ceros(GridEliminados,[Ultimo],GridSuma,Resultado),
-    obtener_columnas(GridSuma,Col1,Col2,Col3,Col4,Col5),
-    
-    renovar_Columna(Col1,ColRen1,NumOfColumns,Grid),
-    renovar_Columna(Col2,ColRen2,NumOfColumns,Grid),
-    renovar_Columna(Col3,ColRen3,NumOfColumns,Grid),
-    renovar_Columna(Col4,ColRen4,NumOfColumns,Grid),
-    renovar_Columna(Col5,ColRen5,NumOfColumns,Grid),
-
-    nueva_lista(ColRen1,ColRen2,ColRen3,ColRen4,ColRen5,NUEVAGRID),
+    obtener_lista_columnas(GridSuma,NumOfColumns,ListaColumnas),
+    obtener_cant_filas(Grid,NumOfColumns,CantidadFilas),
+    renovar_Columnas(ListaColumnas,ListaRenovada,CantidadFilas),
+    intercalar(ListaRenovada,[],NUEVAGRID),
     RGrids=[GridSuma,NUEVAGRID].
     
     
@@ -77,56 +71,91 @@ convertir_en_ceros_aux([Elemento|Resto], Posiciones, PosicionActual, [Elemento|R
     NuevaPosicionActual is PosicionActual + 1,
     convertir_en_ceros_aux(Resto, Posiciones, NuevaPosicionActual, RestoNuevaLista,Valor).
 
+    obtener_lista_columnas(Grilla,NumOfColumns,ListaColumnas):-
+        crear_lista_columnas(NumOfColumns,ListaVacia),
+        obtener_X_Primeros(Grilla,NumOfColumns,X_Primeros),
+        insertar_en_columnas(X_Primeros,ListaVacia,ListaColumnas).
+        
+    insertar_en_columnas([], Resultado, Resultado).
+    insertar_en_columnas([X|Xs], L, Resultado):-
+        insertar_elementos(X, L, ListaAux),
+        insertar_en_columnas(Xs, ListaAux, Resultado).
+    
+    insertar_elementos([], Ls, Ls).
+    insertar_elementos([X|Xs], [L|Ls], [L2|Ls2]) :-
+        append(L, [X], L2),
+        insertar_elementos(Xs, Ls, Ls2).
+    
+    crear_lista_columnas(0, []).
+        crear_lista_columnas(NumOfColumns, [ [] | ListaColumnas ]) :-
+        AUX is NumOfColumns - 1,
+        crear_lista_columnas(AUX, ListaColumnas).
+    
+    obtener_X_Primeros([], _, []).
+    obtener_X_Primeros([H|T], NumOfColumns, [Col|Cols]) :-
+        length(Col, NumOfColumns),
+        append(Col, Rest, [H|T]),
+        obtener_X_Primeros(Rest, NumOfColumns, Cols).
+    
+    renovar_Columnas([],[],_).
+    renovar_Columnas([L|Ls],[L2|Ls2],CantFilas):-
+        eliminar_ceros(L,X),
+        completar_lista(X,L2,CantFilas),
+        renovar_Columnas(Ls,Ls2,CantFilas).
+    
+    completar_lista(Lista, ListaCompleta,CantFilas) :-
+        length(Lista, Longitud),
+        Longitud >= CantFilas,
+        ListaCompleta = Lista.
+    completar_lista(Lista, ListaCompleta,CantFilas) :-
+        length(Lista, Longitud),
+        Longitud < CantFilas,
+        generar_numero_aleatorio(Num),
+        completar_lista([Num|Lista], ListaCompleta,CantFilas).
+        
+     generar_numero_aleatorio(N) :-
+        random_between(1,6, R),
+        obtener_numero(R, N).
+        obtener_numero(1, 2).
+        obtener_numero(2, 4).
+        obtener_numero(3, 8).
+        obtener_numero(4, 16).
+        obtener_numero(5, 32).
+        obtener_numero(6, 64).
+    
+    eliminar_ceros([], []).
+    eliminar_ceros([0|T], L) :- eliminar_ceros(T, L).
+    eliminar_ceros([H|T], [H|L]) :- H \= 0, eliminar_ceros(T, L).
 
-renovar_Columna(Lista,ListaNueva,NumOfColumns,Grid):-
-    eliminar_ceros(Lista,X),
-    completar_lista(X,ListaNueva,NumOfColumns,Grid).
+    intercalar([], Res, R):-
+      reverse(Res,R).
+    intercalar([[]|Xs],Res,R) :-
+    intercalar(Xs,Res,R) .
+    intercalar(L, R1, R) :-
+      L \= [],
+      tomar_cabeceras(L, Cabeceras),
+      reverse(Cabeceras,Cabeceras_Inv),
+      quitar_cabeceras(L,Resto),
+      concatenar(Cabeceras_Inv, R1, RAUX),
+      intercalar(Resto, RAUX, R).
     
-eliminar_ceros([], []).
-eliminar_ceros([0|T], L) :- eliminar_ceros(T, L).
-eliminar_ceros([H|T], [H|L]) :- H \= 0, eliminar_ceros(T, L).
-    
-completar_lista(Lista, ListaCompleta,NumOfColumns,Grid) :-
-    length(Lista, Longitud),
-    obtener_cant_filas(Grid,NumOfColumns,CantFilas),
-    Longitud >= CantFilas,
-    ListaCompleta = Lista.
-completar_lista(Lista, ListaCompleta,NumOfColumns,Grid) :-
-    length(Lista, Longitud),
-    obtener_cant_filas(Grid,NumOfColumns,CantFilas),
-    Longitud < CantFilas,
-    generar_numero_aleatorio(Num),
-    completar_lista([Num|Lista], ListaCompleta,NumOfColumns,Grid).
-    
-generar_numero_aleatorio(N) :-
-    random_between(1,6, R),
-    obtener_numero(R, N).
-    obtener_numero(1, 2).
-    obtener_numero(2, 4).
-    obtener_numero(3, 8).
-    obtener_numero(4, 16).
-    obtener_numero(5, 32).
-    obtener_numero(6, 64).
-    
-obtener_columnas([], [], [], [], [], []).
-obtener_columnas([C1,C2,C3,C4,C5|Resto], [C1|Col1], [C2|Col2], [C3|Col3], [C4|Col4], [C5|Col5]) :-
-    obtener_columnas(Resto, Col1, Col2, Col3, Col4, Col5).
-    
-    
-nueva_lista(L1, L2, L3, L4, L5, Resultado) :-
-    intercalar_listas(L1, L2, L3, L4, L5, [], Resultado).
-    
-intercalar_listas([], [], [], [], [], Resultado, Resultado).
-intercalar_listas([X1|L1], [X2|L2], [X3|L3], [X4|L4], [X5|L5], Temp, Resultado) :-
-    append(Temp, [X1, X2, X3, X4, X5], NuevaLista),
-    intercalar_listas(L1, L2, L3, L4, L5, NuevaLista, Resultado).
 
-long([],0).
-long([_|Xs],L) :- long(Xs,L1), L is L1+1.
-
-obtener_cant_filas(Lista, NumColumnas, NumFilas) :-
-    long(Lista, Longitud),
-    NumFilas is Longitud // NumColumnas.
+    tomar_cabeceras([], []).
+    tomar_cabeceras([[X|_]|Ls], [X|Rs]) :-
+      tomar_cabeceras(Ls, Rs).
+    
+    quitar_cabeceras([], []).
+    quitar_cabeceras([[_|Xs]|Ls], [Xs|Rs]) :-
+      quitar_cabeceras(Ls, Rs).
+    
+    concatenar([], L, L).
+    concatenar([X|L1], L2, [X|L3]) :-
+        concatenar(L1, L2, L3).
+    
+    
+    obtener_cant_filas(Grid, NumColumnas, NumFilas) :-
+        length(Grid, Longitud),
+        NumFilas is Longitud // NumColumnas.    
 
     
 
